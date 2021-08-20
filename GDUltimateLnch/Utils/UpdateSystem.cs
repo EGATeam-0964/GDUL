7101761153c14bf5ec829de121e9e207bffaaa23
Creepy0964 <59210160+Creepy0964@users.noreply.github.com>
2021-08-20 13:24:46 +0300

    Add files via upload

--- GDUltimateLnch/Utils/UpdateSystem.cs
+++ GDUltimateLnch/Utils/UpdateSystem.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Net.Http;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -33,18 +34,45 @@ namespace GDUltimateLnch.Utils
         /// Check version.
         /// </summary>
         /// <returns>A <see cref="bool"/>, which is true if upfate is required.</returns>
+        
+        public static void Ping()
+        {
+            try
+            {
+                Ping myPing = new Ping();
+                String host = "google.com";
+                byte[] buffer = new byte[32];
+                int timeout = 1000;
+                PingOptions pingOptions = new PingOptions();
+                PingReply reply = myPing.Send(host, timeout, buffer, pingOptions);
+                if (reply.Status == IPStatus.Success)
+                {
+                    return;
+                }
+            }
+            catch(System.Net.NetworkInformation.PingException ex)
+            {
+                MessageBox.Show($"Невозможно проверить наличие обновлений. Вы подключены к интернету?",
+                            "Обновление",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Information,
+                            MessageBoxDefaultButton.Button1,
+                            MessageBoxOptions.DefaultDesktopOnly);
+            }
+        }
         public static bool CheckVersion(bool notifyUser = true)
         {
+            Ping();
             Version server = GetServerVersion();
             if (server == null)
             {
                 if (notifyUser)
                     new Task(() =>
                     {
-                        MessageBox.Show($"Невозможно проверить наличие обновлений. Вы подключены к интернету?",
+                        MessageBox.Show($"Ошибка сервера. Проверка обновлений невозможна.",
                             "Обновление",
                             MessageBoxButtons.OK,
-                            MessageBoxIcon.Information,
+                            MessageBoxIcon.Error,
                             MessageBoxDefaultButton.Button1,
                             MessageBoxOptions.DefaultDesktopOnly);
                     }).Start();
